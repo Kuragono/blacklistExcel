230d92fc46feb37a81776f9cac72725157ad8f1f
Kuragono <tiago.carreira@outlook.com>
2022-07-22 11:12:22 +0200

    Add Arrays of the keywords

--- readcsv.js
+++ readcsv.js
@@ -20,11 +20,11 @@ function getData() {
         function readText() {
             let keywordsArray = reader.result.split('\r\n');
             let smallarray = [];
-            for (let i = 0; i <= keywordsArray.length - 1; i++) {
+            for (let i = 0; i < keywordsArray.length; i++) {
                 smallarray.push(keywordsArray[i].split(";"));
             }
-            console.log(smallarray);
             createTable(smallarray);
+            createCounting(smallarray);
         }
 
         
@@ -34,12 +34,35 @@ function getData() {
 function createTable(csv) {
     var body = document.querySelector('body');
     var table = document.createElement('table');
+    table.cellSpacing = 0;
     body.appendChild(table);
     for (let row of csv) {
-        let tr = table.insertRow();
+        let th = table.insertRow();
         for (let col of row) {
-          let td = tr.insertCell();
+          let td = th.insertCell();
           td.innerHTML = col;
         }
     }
+}
+
+function createCounting(csv) {
+    let keyword = '';
+    let keywordlist = [];
+    let bigkeywordlist = [];
+    for (let i = 1; i < csv.length; i++) {
+        for (let j = 0; j < csv[i].length; j++)
+            if (keyword != csv[i][j]) {
+                keyword = csv[i][j];
+                if (keyword != '') {
+                    keywordlist.push(keyword);
+                    bigkeywordlist.push([keyword, [j]]);
+                }
+            } else {
+                if (keyword != '') {
+                    bigkeywordlist.push(j);
+                }
+            } 
+    }
+    console.log(keywordlist);
+    console.log(bigkeywordlist);
 }
